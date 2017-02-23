bfb60450882f0a8ac942f20dc3c992e97afa157c
Chris Stanton <cstanton0760@yahoo.com>
2017-02-23 17:56:11 -0600

    starter code

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 console.log('Starting up the server...');
 
+//var initialLikesCount = 0;
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-
-
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 app.listen(3000);
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function(){
 
+  $.ajax({
+    type:'GET',
+    url: '/bios',
+    success: function(response) {
+    }
+  });//end of ajax
+
+  $.ajax({
+    type:'GET',
+    url: '/likes',
+    success: function(response) {
+    }
+  });//end of ajax
   
-
-
 });//end of doc.ready
