8daaff858cfd53d98777bd65d06a1a47b128a69e
Chris Stanton <cstanton0760@yahoo.com>
2017-02-24 10:02:46 -0600

    fixed sourcing errors

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var bios = require('./routes/bios');
+//var likes = require('./routes/likes');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/routes/bios', bios);//module
-
+//app.use('/routes/likes', likes);//module
 
 app.listen(3000);
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,9 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 
+//GETS
   $.ajax({
-    type:'GET',
+    type: 'GET',
     url: '/bios',
     success: function(response) {
       appendDOM();
@@ -11,31 +12,33 @@ $(document).ready(function(){
 
 $('#likeButton').on('click', function(){
   $.ajax({
-    type:'GET',
+    type: 'GET',
     url: '/likes',
     success: function(response) {
       appendDOM();
     }
   });//end of ajax
 
+//POSTS
   $.ajax({
     type: 'POST',
     url: '/likes',
-    data: ,
+    //data: ,
     success: function() {
     }
-  });
-}//end of on click
+  });//end of ajax
+})//end of on click
 
+//functions()
 function appendDOM() {
   for (var i = 0; i < response.length; i++) {
     $('#container').append(
       '<h1>' + '<img src="'+ response[i].image + '"/>' + //image
-        '<div id="name">' + response[i].name + '</div>' //name
-        '<div>' + response[i].bio + '</div>' //bio
-        '<button id="likeButton"><span' + response[i].name + '></span></button>'//button
+        '<div id="name">' + response[i].name + '</div>' +//name
+        '<div>' + response[i].bio + '</div>' +//bio
+        '<button id=" likeButton"><span' + response[i].name + '></span></button>' +//button
       '</h1>'
     );
   }//ends our for loop
-}
+}//end of appendDOM()
 });//end of doc.ready
